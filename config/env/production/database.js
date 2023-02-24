d56eb27a762604422b0e4946162228ca7e1dcbbe
Tayseer777 <tayseer@vuedale.com>
2023-02-24 15:58:24 +0300

    GG

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,6 @@
 const {parse} =  require('pg-connection-string')
 module.exports = ({ env }) => {
-    const {host, port, database, user, password} = parse(env('HEROKU_POSTGRESQL_GOLD_URL'))
+    const {host, port, database, user, password} = parse(env('DATABASE_URL'))
     return({
     connection: {
       client: 'postgres',
